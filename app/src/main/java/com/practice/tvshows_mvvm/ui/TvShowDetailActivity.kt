c9d8395b996cadb69007ec954abe5dec7a6222d3
Lucas Ferreira <lucasfteo@gmail.com>
2023-01-23 18:16:10 -0300

    SetUp TvShowDetailsActivity

--- app/src/main/java/com/practice/tvshows_mvvm/ui/TvShowDetailActivity.kt
+++ app/src/main/java/com/practice/tvshows_mvvm/ui/TvShowDetailActivity.kt
@@ -2,10 +2,14 @@ package com.practice.tvshows_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import coil.load
 import com.practice.tvshows_mvvm.databinding.ActivityTvShowDetailBinding
 import com.practice.tvshows_mvvm.viewmodel.TvShowViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TvShowDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTvShowDetailBinding
@@ -15,12 +19,28 @@ class TvShowDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityTvShowDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        supportActionBar?.hide()
         loadTvShow()
     }
 
     private fun loadTvShow() {
         val id = intent.getIntExtra("id", 0)
         viewModel.getTvShow(id)
+        observer()
+    }
+
+    private fun observer() = with(binding) {
+        viewModel.responseTvShow.observe(this@TvShowDetailActivity) {tvShow ->
+            tvShow?.let {
+                progressBar.visibility = View.GONE
+                tvShowName.visibility = View.VISIBLE
+                tvShowImage.visibility = View.VISIBLE
+                tvShowName.text = tvShow.name
+                tvShowImage.load(tvShow.image.original) {
+                    crossfade(true)
+                    crossfade(1000)
+                }
+            }
+        }
     }
 }
