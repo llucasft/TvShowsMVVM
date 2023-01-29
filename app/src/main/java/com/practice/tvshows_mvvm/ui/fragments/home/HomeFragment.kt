9a20cbbd0a2a11bcee3fa86787ae277c84355b11
Lucas Ferreira <lucasfteo@gmail.com>
2023-01-29 17:50:39 -0300

    Setup HomeFragment

--- app/src/main/java/com/practice/tvshows_mvvm/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/practice/tvshows_mvvm/ui/fragments/home/HomeFragment.kt
@@ -1,13 +1,55 @@
 package com.practice.tvshows_mvvm.ui.fragments.home
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.practice.tvshows_mvvm.adapter.TvShowAdapter
 import com.practice.tvshows_mvvm.databinding.FragmentHomeBinding
+import com.practice.tvshows_mvvm.models.TvShowItem
 import com.practice.tvshows_mvvm.ui.fragments.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+
     override val viewModel: HomeViewModel by viewModels()
+    private lateinit var tvShowAdapter: TvShowAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observers()
+//        setupFavoritesButton()
+    }
+
+    private fun observers() {
+        viewModel.responseTvShow.observe(viewLifecycleOwner) { listTvShows ->
+            if (listTvShows.isNotEmpty()) {
+                setUpRv(listTvShows)
+                binding.progressBar.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setUpRv(listTvShows: List<TvShowItem>) {
+        tvShowAdapter = TvShowAdapter(listTvShows)
+
+        binding.recyclerView.apply {
+            adapter = tvShowAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        tvShowAdapter.setOnItemClickListener(object : TvShowAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                val action = HomeFragmentDirections
+                    .actionHomeFragmentToDetailsFragment(listTvShows[position].id)
+                findNavController().navigate(action)
+            }
+        })
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
--- app/src/main/java/com/practice/tvshows_mvvm/ui/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/practice/tvshows_mvvm/ui/fragments/home/HomeViewModel.kt
@@ -1,12 +1,36 @@
 package com.practice.tvshows_mvvm.ui.fragments.home
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.practice.tvshows_mvvm.models.TvShowItem
 import com.practice.tvshows_mvvm.repository.TvShowRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val repository: TvShowRepository
 ): ViewModel() {
+    private val _response =  MutableLiveData<List<TvShowItem>>()
+    val responseTvShow: LiveData<List<TvShowItem>>
+        get() = _response
+
+    init {
+        getAllTvShows()
+    }
+
+    private fun getAllTvShows() = viewModelScope.launch {
+        repository.getTvShows().let { response ->
+            if (response.isSuccessful){
+                _response.value = response.body()
+                Log.i("TvShowViewModel", "getAllTvShows Success: ${response.body()}")
+            } else {
+                Log.i("TvShowViewModel", "getAllTvShows Error: ${response.code()}")
+            }
+        }
+    }
 }
